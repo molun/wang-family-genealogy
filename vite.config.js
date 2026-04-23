93ac0a6b81cecfae8eb09da3c1f51fe08bd63f60
molun <xinhiman@126.com>
2026-04-23 23:57:53 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+
 export default defineConfig({
   plugins: [vue()],
-  base: './',
+  // 关键：和你的仓库名保持一致，前后加斜杠
+  base: '/wang-family-genealogy/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
